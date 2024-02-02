972739317098c511360a8888ac4dad87c9c96584
GRIGORY DEV <r890415@gmail.com>
2024-02-02 05:23:03 +0200

    modified:   back/index.js 	modified:   src/App.jsx

--- back/index.js
+++ back/index.js
@@ -10,6 +10,7 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('dist'))
 
 let persons = [
     { 
